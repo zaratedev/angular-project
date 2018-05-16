3f3da4e3666b643bc97c4194144f138ea3b9aab7
zaratedev <zaratedev@gmail.com>
2018-05-16 17:55:58 -0500

    My first component angular

---
+++ src/app/title/title.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'title',
+  templateUrl: 'title.component.html'
+})
+export class TitleComponent {
+  public name : string = 'Curso';
+}
