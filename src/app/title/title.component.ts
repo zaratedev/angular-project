4744beed2fd4dd5d792776cf376d9a023dfe9e93
zaratedev <zaratedev@gmail.com>
2018-05-16 18:29:36 -0500

    Implements ngOnInit

--- src/app/title/title.component.ts
+++ src/app/title/title.component.ts
@@ -1,9 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'title',
   templateUrl: 'title.component.html'
 })
-export class TitleComponent {
-  public name : string = 'Curso';
+export class TitleComponent implements OnInit{
+  public name : string;
+  public edad : number;
+  public edades : number[];
+
+  constructor() {}
+
+  // Se ejecuta cuando el componente ha sido inicializado
+  ngOnInit() {
+    this.name = 'Jonathan';
+    this.edad = 24;
+  }
+
+  getEdadNombre() : string {
+    return `${this.name} : ${this.edad}`;
+  }
 }
