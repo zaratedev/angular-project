708318955f9ac7bf93908d86fefc2fbe7943abb1
zaratedev <zaratedev@gmail.com>
2018-05-17 12:48:38 -0500

    Styles

--- src/app/title/title.component.ts
+++ src/app/title/title.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'title',
-  templateUrl: 'title.component.html'
+  templateUrl: 'title.component.html',
+  styleUrls: ['title.component.css']
 })
 export class TitleComponent implements OnInit{
   public name : string;
